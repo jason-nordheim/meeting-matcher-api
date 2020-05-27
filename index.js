3fbbb6fd42ece840ec4a79721629065e1a8e33b8
jason-nordheim <jason.nordheim@gmail.com>
2020-05-26 18:48:47 -0600

    able to create a user with a password digest and send a user their token

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // constants 
 const PORT = process.env.PORT || 4000; 
+const SECRET = 'TestSecret'; 
 
 // imports 
+const database = require('./database'); 
 const express = require('express'); 
 const cors = require('cors'); 
 const bodyParser = require('body-parser'); 
-const database = require('./database'); 
+const bcrypt = require('bcrypt'); 
+const jwt = require('jsonwebtoken'); 
 
 // setup 
 const app = express(); 
@@ -14,9 +17,17 @@ app.use(bodyParser.json())
 
 // route functions 
 const createUser = (request, response) => {
-    response.json({
-        user: "you added a user, yay!"
-    }); 
+    const {username, password } = request.body; 
+    const securityLevel = 12; 
+    bcrypt.hash(password, securityLevel)
+    .then(hashedPassword => {
+        return database("user").insert({
+            username: username, password_digest: hashedPassword
+        }).returning('*') // return all results 
+        .then(users => {
+            response.status(200).json({user: users[0]}); 
+        })
+    })
 }
 const getUsers = (request, response) => {
     database("user").select()
@@ -26,10 +37,33 @@ const getUsers = (request, response) => {
         }); 
     });
 }
+const login = (request, response) => {
+    const {username, password } = request.body; 
+    //lookup user 
+    database("user").select().where({username: username}).first()
+    .then(user => {
+        if(!user) throw new Error("Invalid username"); 
+        
+        // check if the provided password hashes to the password digest
+        return bcrypt.compare(password, user.password_digest) 
+        .then(passwordMatched => {
+            if(!passwordMatched) throw new Error("Invalid Password")
+            return user; 
+        });
+    }).then(user => { 
+        // generate token, because password matches 
+        jwt.sign(user, SECRET, (error ,token) => {
+            if(error) throw new Error("Problem signing JWT"); 
+            response.json({token}); 
+        }); 
+    })
+    .catch(error => response.status(401).json({error: error.message})); 
+}
 
 // routes 
 app.post('/users', createUser)
 app.get('/users', getUsers)
+app.post('/login', login)
 
 // listen 
 app.listen(PORT, ()=> console.log(`Listeing on port ${PORT}...`))
