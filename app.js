12bc30eaa6e74179999acbb10ee5984e1ae7033c
jason-nordheim <jason.nordheim@gmail.com>
2020-05-28 15:04:19 -0600

    added route for getting meetings leveraging tokens

--- app.js
+++ app.js
@@ -9,14 +9,31 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken'); 
 const bodyParser = require('body-parser')
 const express = require('express')
-const db = require('./config/database')
-//const UserRouter = require('./routes/users') 
+const db = require("./config/database"); 
 
-// setup 
+//const UserRouter = require('./routes/users') // not needed for now 
+
+/****** setup ******/ 
+
 const app = express()
 app.use(cors())
 app.use(bodyParser.json()) 
 
+
+const lookupMeetings = (request, response, next) => {
+    const authorization = request.headers.authorization; 
+    if (authorization) {
+        const encodedToken = authorization.split(' ')[1]
+        const decodedToken = jwt.decode(encodedToken) 
+        const currentUser = { 
+            id: decodedToken.id, 
+            username: decodedToken.username 
+        } 
+        console.log(currentUser) 
+    } else {
+        response.sendStatus(403).json({error: "Unauthorized"})
+    }
+}
 
 const createUser = (req, res, next) => {
     const {username, password} = req.body; 
@@ -31,36 +48,48 @@ const createUser = (req, res, next) => {
             })
             .returning("*")
             .then(users => {
-                console.log(users)
-                res.sendStatus(200) 
+                //res.sendStatus(200).json({ result: users[0]})
+                loginUser(username,password, res)
             })
         })
 }
-const loginUser = (req, res, next) => {
-    const {username, password} = req.body; 
+
+const loginUser = (username, password, response) => {
     db("user")
-        .select()
-        .where({username: username})
-        .first()
-        .then(user => {
-            if(!user) throw new Error("Invalid Username")
-            /* check to see if their password matches, returns true/false */ 
-            return bcrypt.compare(password, user.password_digest) 
-            .then(passwordMatches => {
-                if(!passwordMatches) throw new Error("Invalid Password") 
-                /* password matches */ 
-                jwt.sign(user, config.secret, (error, token) => {
-                    if(error) throw new Error("Unable to sign") 
-                    res.json({token}); 
-                })
+    .select()
+    .where({username: username})
+    .first()
+    .then(user => {
+        if(!user) throw new Error("Invalid Username")
+        /* check to see if their password matches, returns true/false */ 
+        return bcrypt.compare(password, user.password_digest) 
+        .then(passwordMatches => {
+            if(!passwordMatches) throw new Error("Invalid Password") 
+            /* password matches */ 
+            jwt.sign(user, config.secret, (error, token) => {
+                if(error) throw new Error("Unable to sign") 
+                response.json({token}); 
             })
         })
-        .catch(error => res.sendStatus(401).json({error})) // unauthorized 
+    })
+    .catch(error => response.sendStatus(401).json({error})) // unauthorized 
 }
 
-app.post("/users", createUser);
-app.post("/login", loginUser)
+const loginUserRoute = (req, res, next) => {
+    const {username, password} = req.body; 
+    loginUser(username, password, res) 
+}
 
+
+/* Routes */ 
+app.post("/users", createUser);
+app.post("/login", loginUserRoute)
+app.get('/meetings', lookupMeetings)
+
+
+
+
+/* Start listening */ 
 app.listen(
     config.port, 
     ()=> console.log(`Meeting Matcher API started. Listening on port ${config.port}...`)
